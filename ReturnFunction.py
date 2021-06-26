1af9bc8e4305778b37b072db7112cf99b02e2119
sweety1201 <84372592+sweety1201@users.noreply.github.com>
2021-06-25 22:49:11 -0600

    Create ReturnFunction.py

---
+++ ReturnFunction.py
@@ -0,0 +1,20 @@
+#function with return expression
+def greeting():
+    print("HI")
+    return("Good Afternoon Mam")
+#call the greeting()
+#variable= fuction_name()
+result=greeting()
+print(result)
+#secondway
+#call and println
+print("secondway of Calling")
+print("HI")
+print("Good Afternoon Mam")
+
+OUTPUT:
+    HI
+Good Afternoon Mam
+secondway of Calling
+HI
+Good Afternoon Mam
